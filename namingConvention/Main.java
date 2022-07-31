0d565fe237a1ef300da767663cef9e45c7b4ae6b
Alperen Öztürk <78054802+AlperenOztrkk@users.noreply.github.com>
2022-07-31 20:36:35 +0300

    Ekleme

---
+++ namingConvention/Main.java
@@ -0,0 +1,20 @@
+package namingConvention;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+	 public class ProductManager{
+		 private int enBuyukSayi;
+			public  ProductManager() {
+				
+			}
+			public void getAllByCategoryId(int categoryId) {
+				int enKucukSayi;
+			}
+	 }
+	
+
+
+}
